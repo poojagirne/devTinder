54f00abc2f895be0853649eb315f656999bab165
poojagirne <poojagirne24@gmail.com>
2025-08-16 17:53:14 +0530

    Added nodemon

--- src/app.js
+++ src/app.js
@@ -2,4 +2,10 @@ const express=require("express")
 
 const app=express()
 
-app.listen(3000)
+app.get("/user",(req,res,next)=>{
+    console.log("Handling the route")
+    res.send("hello")
+})
+app.listen(3000,()=>{
+    console.log("Server is connected successfully")
+})
