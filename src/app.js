935eff893b6c4982a47ed227849b78d4ec93bc63
poojagirne <poojagirne24@gmail.com>
2025-08-18 19:14:07 +0530

    Get user details

--- src/app.js
+++ src/app.js
@@ -6,15 +6,27 @@ const app = express();
 app.use(express.json());
 
 app.post("/signUp", async (req, res) => {
-    //creating a new instance of user model
-    const user=new User(req.body)
-    try {
-      await user.save()
-      res.send("User Added Succesfully")
-    } catch (error) {
-        res.status(400).send("error saving the user" +error)
-    }
+  //creating a new instance of user model
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("User Added Succesfully");
+  } catch (error) {
+    res.status(400).send("error saving the user" + error);
+  }
 });
+
+app.get("/user", async (req, res) => {
+  try {
+    const userEmail = req.body.emailId;
+    console.log(userEmail)
+    const user =await User.find({ emailId: userEmail });
+    res.send(user)
+  } catch (error) {
+    res.status(400).send("not found");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection established...");
